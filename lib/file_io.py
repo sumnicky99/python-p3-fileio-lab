da5c5757486a086eadfe936b105cae1e838f5131
Sumnicky99 <sumnicky99@gmail.com>
2024-06-02 10:51:32 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,25 @@
 def write_file(file_name, file_content):
-    pass
+    # Convert file_name to a string and concatenate ".txt" extension to it
+    file_name = str(file_name) + ".txt"
+    # Open the file in write mode ('w') with utf-8 encoding
+    with open(file_name, mode='w', encoding='utf-8') as file:
+        # Write the provided content to the file
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    # Convert file_name to a string and concatenate ".txt" extension to it
+    file_name = str(file_name) + ".txt"
+    # Open the file in append mode ('a') with utf-8 encoding
+    with open(file_name, mode='a', encoding='utf-8') as file:
+        # Append the provided content to the file
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    # Convert file_name to a string and concatenate ".txt" extension to it
+    file_name = str(file_name) + ".txt"
+    # Open the file in read mode ('r') with utf-8 encoding
+    with open(file_name, mode='r', encoding='utf-8') as file:
+        # Read the content of the file and return it
+        return file.read()
